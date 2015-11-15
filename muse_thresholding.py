26ee2faa8c2585a9d648451901f9fe80a5058ac8
joeylmaalouf <joeylmaalouf@gmail.com>
2015-11-14 20:00:51 -0500

    sleep counter to limit event firing rate

--- muse_thresholding.py
+++ muse_thresholding.py
@@ -4,22 +4,37 @@ import time
 
 
 class MuseServer(ServerThread):
-  def __init__(self, port = 5001, mean = 850, deviation = 80):
+  def __init__(self, port = 5001, mean = 850, deviation = 80, sleep_timer = 200):
     ServerThread.__init__(self, port)
     self.mean = mean
     self.deviation = deviation
+    self.sleep_timer = sleep_timer
+    self.sleep_counter = 0
+    self.sleeping = False
 
   @make_method("/muse/eeg", "ffff")
   def eeg_callback(self, path, args):
     (l_ear, l_forehead, r_forehead, r_ear) = args
-    if r_ear < (self.mean - self.deviation):
-      print("Up")
-    elif r_ear > (self.mean + self.deviation):
-      print("Down")
-    if l_forehead < (self.mean - self.deviation):
-      print("Right")
-    elif l_forehead > (self.mean + self.deviation):
-      print("Left")
+
+    if self.sleep_counter >= self.sleep_timer:
+      self.sleep_counter = 0
+      self.sleeping = False
+
+    if not self.sleeping:
+      if r_ear < (self.mean - self.deviation):
+        print("Up")
+        self.sleeping = True
+      elif r_ear > (self.mean + self.deviation):
+        print("Down")
+        self.sleeping = True
+      if l_forehead < (self.mean - self.deviation):
+        print("Right")
+        self.sleeping = True
+      elif l_forehead > (self.mean + self.deviation):
+        print("Left")
+        self.sleeping = True
+    else:
+      self.sleep_counter += 1
 
 
 if __name__ == "__main__":
